de7b3d203b75b672b3a4e7dc63801c15d3abee4a
LOCO185 <jloco151@gmail.com>
2022-09-25 22:02:38 +0300

    Changes on Exclusive

--- src/components/Exclusive/Exclusive.jsx
+++ src/components/Exclusive/Exclusive.jsx
@@ -1,10 +1,7 @@
 import React from "react";
+import "./Exclusive.css";
 
 const Exclusive = () => {
-    return (
-        <div className="Exclusive">
-            hi iam Exclusive
-        </div>
-    );
-}
+  return <div className="Exclusive">hi iam Exclusive</div>;
+};
 export default Exclusive;
