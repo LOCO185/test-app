28e63c93f39d398cc6555a37fcd3199e0d9238b2
LOCO185 <jloco151@gmail.com>
2022-09-26 19:22:06 +0300

    Added Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import './style.css';
+import "./style.css";
 // import Cart from "../Cart/Cart";
 // import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div>
-                <a href="#/">
-                    <h1>Clothing App</h1>
-                </a>
-            </div>
-            <div className="right">
-            <a href="#/shop">Shop</a><a href="#/categories">Categories</a><a href="#/signin">Sign In</a><a href="#/cart">Cart ()</a>
-            </div>
-        </header>
-    );
-}
+  return (
+    <header className="header">
+      <div>
+        <a href="#/">
+          <h1>Clothing App</h1>
+        </a>
+      </div>
+      <div className="right">
+        <a href="#/shop">Shop</a>
+        <a href="#/categories">Categories</a>
+        <a href="#/signin">Sign In</a>
+        <a href="#/cart">Cart ()</a>
+      </div>
+    </header>
+  );
+};
 export default Header;
