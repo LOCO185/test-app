e6b5b4b1570ef9e8ef9abc2c1ff6af29cb5c3c9a
LOCO185 <jloco151@gmail.com>
2022-09-26 19:24:00 +0300

    Added Womens component

--- src/components/Womens/Womens.jsx
+++ src/components/Womens/Womens.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
 const Womens = () => {
-    return (
-        <div className="Womens">
-            hi iam Womens
-        </div>
-    );
-}
+  return <div className="Womens">hi iam Womens</div>;
+};
 export default Womens;
