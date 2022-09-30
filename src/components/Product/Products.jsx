c0b2b7f876e4991b7252b4f71a3ecede0ddc46c8
LOCO185 <jloco151@gmail.com>
2022-09-30 14:59:37 +0300

    Added Styles ro Products

--- src/components/Product/Products.jsx
+++ src/components/Product/Products.jsx
@@ -2,6 +2,13 @@ import styled from "styled-components";
 import { popularProducts } from "../Data/Data";
 import Product from "./Product";
 
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
 const Products = () => {
   return <div>Products</div>;
 };
