648014655dc57170428570de27730fd9e80f4afc
LOCO185 <jloco151@gmail.com>
2022-09-30 15:06:16 +0300

    Imports mui icons & styled components

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import styled from "styled-components";
 
 const Product = () => {
   return <div>Product</div>;
