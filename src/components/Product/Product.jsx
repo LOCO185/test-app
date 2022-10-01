528315045de94c5369f2d781975810a175839218
LOCO185 <jloco151@gmail.com>
2022-10-01 21:12:08 +0300

    Added styles to container on Product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -19,6 +19,20 @@ const Info = styled.div`
   cursor: pointer;
 `;
 
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
 
 const Product = ({ item }) => {
   return (
