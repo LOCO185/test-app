a0be4360fe95f3730655ca17ea7ed829fa22b58a
LOCO185 <jloco151@gmail.com>
2022-10-02 19:31:55 +0300

    Added styles to container

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 // import mobile responsive
 
+const Container = styled.div`
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+
 const Footer = () => {
   return <div>Footer</div>;
 };
