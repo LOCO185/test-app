ca214ef4ea1ecbac7cea79bd7292ea9a610b9fcb
LOCO185 <jloco151@gmail.com>
2022-10-03 19:46:56 +0300

    Added items into list

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -97,6 +97,21 @@ const Footer = () => {
           </SocialIcon>
         </SocialContainer>
       </Left>
+      <Center>
+        <Title>Useul Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
     </Container>
   );
 };
