65650c2df994af337f31738884c72c2cb220dc53
LOCO185 <jloco151@gmail.com>
2022-10-04 23:12:43 +0300

    Imports

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Badge } from "@material-ui/core";
-import SearchIcon from '@mui/icons-material/Search';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import styled from "styled-components";
+import { mobile } from "../../responsive";
 
 const Navbar = () => {
   return <div>Navbar</div>;
