df6ebf79626624cf6e4009abf415ff472a5381af
LOCO185 <jloco151@gmail.com>
2022-10-04 23:11:49 +0300

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Badge } from "@material-ui/core";
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
