a32b7fd524c79827d29c0fc6a6c916cad2146ca6
LOCO185 <jloco151@gmail.com>
2022-10-06 17:42:47 +0300

    Added styles to Cotainer

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -3,6 +3,15 @@ import styled from "styled-components";
 import { mobile } from "../../responsive";
 import SendIcon from '@mui/icons-material/Send';
 
+const Container = styled.div`
+  height: 60vh;
+  background-color: #fcf5f5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
 const NewsLetter = () => {
   return <div>NewsLetter</div>;
 };
