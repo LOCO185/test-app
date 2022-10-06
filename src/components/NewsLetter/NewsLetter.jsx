a9c5e5b3dc47c1a684d2f5eea8cf60f93bb6dc27
LOCO185 <jloco151@gmail.com>
2022-10-06 22:48:44 +0300

    Added InputContianer

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -52,6 +52,12 @@ const NewsLetter = () => {
     <Container>
       <Title>Newsletter</Title>
       <Desc>Get timely updates from your favorite products.</Desc>
+      <InputContainer>
+        <Input placeholder="Your email" />
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
     </Container>
   );
 };
