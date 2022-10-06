4b5bea028309b9e6e29f26b896e4a37e2f5f9e80
LOCO185 <jloco151@gmail.com>
2022-10-06 17:36:07 +0300

    Imported mui icons

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { mobile } from "../../responsive";
+import SendIcon from '@mui/icons-material/Send';
 
 const NewsLetter = () => {
   return <div>NewsLetter</div>;
