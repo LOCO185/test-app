d0c6c04f5e947844577af654b529fa9672f3144d
LOCO185 <jloco151@gmail.com>
2022-10-10 19:27:17 +0300

    styles for Container

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -5,6 +5,15 @@ import { mobile } from "../../responsive";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+  ${mobile({ display: "none" })}
+`;
+
 const Slider = () => {
   return <div>Slider</div>;
 };
