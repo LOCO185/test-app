bb61228f1ffe5356c361ef20600dee7e8bf40c50
LOCO185 <jloco151@gmail.com>
2022-10-10 19:24:01 +0300

    Import mui/icons

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styled from "styled-components";
 import sliderItems from "../../Data";
 import { mobile } from "../../responsive";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 
 const Slider = () => {
   return <div>Slider</div>;
