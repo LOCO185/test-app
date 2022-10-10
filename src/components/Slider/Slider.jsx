cc3a6038d240f54a9dd5dfc46264d08fb78ce8bc
LOCO185 <jloco151@gmail.com>
2022-10-10 19:27:49 +0300

    styles for Arrow

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -14,6 +14,25 @@ const Container = styled.div`
   ${mobile({ display: "none" })}
 `;
 
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
 const Slider = () => {
   return <div>Slider</div>;
 };
