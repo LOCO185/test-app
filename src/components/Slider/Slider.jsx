5d057b837fc36c1afb5b5c87a1e3739f43cf9bfd
LOCO185 <jloco151@gmail.com>
2022-10-10 19:22:07 +0300

    Imports mobile responsive

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
-
+import sliderItems from "../../Data";
+import { mobile } from "../../responsive";
 
 const Slider = () => {
   return <div>Slider</div>;
