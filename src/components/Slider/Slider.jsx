962008455154df9308c149e84b05d96c87dab414
LOCO185 <jloco151@gmail.com>
2022-10-11 23:12:01 +0300

    Slider done

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -89,6 +89,7 @@ const Slider = () => {
       setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
     }
   };
+  
   return (
     <Container>
       <Arrow direction="left" onClick={() => handleClick("left")}>
