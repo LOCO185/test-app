8b85985daf6d8ec40bf6361b063086d900ae00bf
LOCO185 <jloco151@gmail.com>
2022-10-12 19:54:48 +0300

    Added styles to Conatainer

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,21 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
 
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function Register() {
   return <div>Register</div>;
 }
