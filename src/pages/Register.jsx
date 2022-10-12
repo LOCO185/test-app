499d96f93a76eb31e5ddcec0caf0bb13e80b1a59
LOCO185 <jloco151@gmail.com>
2022-10-12 19:55:29 +0300

    Added styles to Title

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -23,6 +23,11 @@ const Wrapper = styled.div`
   ${mobile({ width: "75%" })}
 `;
 
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
 function Register() {
   return <div>Register</div>;
 }
