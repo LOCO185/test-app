0ffb7425151ea978021ec4c0dccf797beae8524a
LOCO185 <jloco151@gmail.com>
2022-10-12 19:47:38 +0300

    Imported components into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import Categories from "../components/Categories/Categories";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import NewsLetter from "../components/NewsLetter/NewsLetter";
+import Products from "../components/Products/Products";
+import Slider from "../components/Slider/Slider";
 
 const Home = () => {
   return <div>Home</div>;
