979a0bf9ff188fd00dae97684789bf648da39eaa
LOCO185 <jloco151@gmail.com>
2022-10-14 20:35:58 +0300

    styles for Button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,6 +40,16 @@ const Input = styled.input`
   padding: 10px;
 `;
 
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
 const Login = () => {
   return <div>Login</div>;
 };
