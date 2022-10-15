9f8794f192731a911c4f7450fef50e7e34c2be5d
LOCO185 <jloco151@gmail.com>
2022-10-15 21:48:34 +0300

    Created Title

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import { mobile } from "../responsive";
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 const Container = styled.div``;
 
@@ -12,12 +12,15 @@ const Wrapper = styled.div`
   ${mobile({ padding: "10px" })}
 `;
 
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+
 const Cart = () => {
   return (
     <Container>
-      <Wrapper>
-        
-      </Wrapper>
+      <Wrapper></Wrapper>
     </Container>
   );
 };
