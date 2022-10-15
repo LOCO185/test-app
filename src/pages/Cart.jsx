28fca9f27005a3461de7521d3e6f31e462a540b4
LOCO185 <jloco151@gmail.com>
2022-10-15 21:43:59 +0300

    Import styled components & components

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import styled from "styled-components";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import { mobile } from "../responsive";
 
 const Cart = () => {
   return <div>Cart</div>;
