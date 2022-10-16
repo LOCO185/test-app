b14e24017b21b22dfbe1742c9294d1db4f47b1b3
LOCO185 <jloco151@gmail.com>
2022-10-16 16:57:31 +0300

    stlyes for Price

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -86,6 +86,16 @@ const ProductColor = styled.div`
   background-color: ${(props) => props.color};
 `;
 
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 const Cart = () => {
   return (
     <Container>
