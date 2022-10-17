01a3bba1d60dee52459692dcf76edb4530069a8d
LOCO185 <jloco151@gmail.com>
2022-10-17 19:55:54 +0300

    Added Price Detail  and Amount

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -169,7 +169,7 @@ const Cart = () => {
         <Bottom>
           <Info>
             <Product>
-              <PriceDetail>
+              <ProductDetail>
                 <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
                 <Details>
                   <ProductName>
@@ -183,13 +183,17 @@ const Cart = () => {
                     <b>Size:</b> 37.5
                   </ProductSize>
                 </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
               </PriceDetail>
-              <ProductAmountContainer>
-                <Add />
-                <ProductAmount>2</ProductAmount>
-                <Remove />
-              </ProductAmountContainer>
             </Product>
+            {/* Here */}
           </Info>
         </Bottom>
       </Wrapper>
