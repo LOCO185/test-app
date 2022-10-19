93ccc382bd9e1b7bca69fb03d20bd26ccf569d4f
LOCO185 <jloco151@gmail.com>
2022-10-19 18:08:59 +0300

    Added Container styles

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,6 +4,8 @@ import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import NewsLetter from "../components/NewsLetter/NewsLetter";
 
+const Container = styled.div``;
+
 const Product = () => {
   return <div>Product</div>;
 };
