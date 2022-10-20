23ecf095108a73aabb428d1348b68a508800eb16
LOCO185 <jloco151@gmail.com>
2022-10-20 21:36:55 +0300

    Added FilterSize

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -69,6 +69,11 @@ const FilterColor = styled.div`
   cursor: pointer;
 `;
 
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
 const Product = () => {
   return <div>Product</div>;
 };
