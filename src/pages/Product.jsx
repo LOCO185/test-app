ea6fe45850ff036bf6b7cde17fac95f6e4f311d3
LOCO185 <jloco151@gmail.com>
2022-10-23 23:38:51 +0300

    ..

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { mobile } from "../responsive";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
-import NewsLetter from "../components/NewsLetter/NewsLetter";
 
 const Container = styled.div``;
 
