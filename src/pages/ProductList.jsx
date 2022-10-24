a8a9a091610805eae54ceeb3cc3a609d8d1f87bf
LOCO185 <jloco151@gmail.com>
2022-10-24 18:23:36 +0300

    Import mobile responsive

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar/Navbar";
 import Products from "../components/Products/Products";
 import NewsLetter from "../components/NewsLetter/NewsLetter";
 import Footer from "../components/Footer/Footer";
+import { mobile } from "../responsive";
 
 const ProductList = () => {
   return <div>ProductList</div>;
