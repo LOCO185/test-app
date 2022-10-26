5769cd3b559eb97f09ce2c2f067b7f79b5fa41d4
LOCO185 <jloco151@gmail.com>
2022-10-26 22:16:19 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import "./App.css";
-import HomePage from "./components/HomePage/HomePage";
-import Product from "./Product";
-import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Product />
-      </BrowserRouter>
-      <HomePage />
-    </div>
+
   );
 }
 
