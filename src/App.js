08fc81c535d9f50b18521bc778bd4212d0883c91
LOCO185 <jloco151@gmail.com>
2022-10-26 22:19:43 +0300

    Import Register

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 function App() {
   return <Home />;
