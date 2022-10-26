d9644f7c220737ce70791e9c0e7909012c6f7dd7
LOCO185 <jloco151@gmail.com>
2022-10-26 22:21:11 +0300

    Import Cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 function App() {
   return <Home />;
