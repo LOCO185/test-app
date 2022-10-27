7abb76447ee2346c42b76d96867048f4890b187f
LOCO185 <jloco151@gmail.com>
2022-10-27 19:37:13 +0300

    commit

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,4 @@
 import React from "react";
-import { Redirect, Switch } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Exclusive from "./components/Exclusive/Exclusive";
-import Shops from "./components/Shops/Shops";
-import Mens from "./components/Mens/Mens";
-import Womens from "./components/Womens/Womens";
 
 
 function Product() {
