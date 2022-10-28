c5753e41b6d2e6f163f836bbab25ebef4da3e904
LOCO185 <jloco151@gmail.com>
2022-10-28 22:27:35 +0300

    Fixed errors

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
-import sliderItems from "../../Data/Data";
+import { sliderItems } from "../../Data/Data";
 import { mobile } from "../../responsive";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
