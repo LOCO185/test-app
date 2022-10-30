fe8a880bb7ead1ccf7f2abaf416cef60eb36c064
LOCO185 <jloco151@gmail.com>
2022-10-30 21:50:18 +0200

    Fixes on Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Badge } from "@material-ui/core";
-import SearchIcon from "@mui/icons-material/Search";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import styled from "styled-components";
 import { mobile } from "../../responsive";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
