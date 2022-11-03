fd153f9f9020bcbe4a55de70c278a18f028e00c3
LOCO185 <jloco151@gmail.com>
2022-11-03 18:13:03 +0200

    Added Routes for register and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import Product from "./pages/Product";
 // import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
+import Register from "./pages/Register";
 // import Login from "./pages/Login";
 // import Cart from "./pages/Cart";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
